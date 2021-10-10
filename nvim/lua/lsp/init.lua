8a48a159bf23a5511e348c2781af318bd4f92e52
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2021-10-10 11:11:59 -0300

    Add tsserver to lsps

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -35,7 +35,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local nvim_lsp = require('lspconfig')
-local servers = { 'pyright', 'gopls' }
+local servers = { 'pyright', 'gopls', 'tsserver' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
