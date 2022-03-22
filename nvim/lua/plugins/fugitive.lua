ef29a1003212a1cf047d8a2e8b82d7d29f9d83a9
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2022-03-22 06:26:42 -0300

    Update fugitive keymaps

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,8 @@
 require'keymaps.util'
 
 map('n', '<leader>gs', ':G<cr>')  -- git status
-map('n', '<leader>gh', ':diffget //2<cr>')  -- select left
-map('n', '<leader>gl', ':diffget //3<cr>')  -- select right
+map('n', '<leader>gd', ':Gvdiffsplit!<cr>') -- resolve merge conflicts
+map('n', 'gh', ':diffget //2<cr>')  -- select left
+map('n', 'gl', ':diffget //3<cr>')  -- select right
 map('n', '<leader>gc', ':Git commit<cr>')  -- git commit
 map('n', '<leader>gp', ':Git push -u origin HEAD<cr>')  -- git push
