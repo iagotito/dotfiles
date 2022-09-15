2e74e39c8cc2d7e4ada9962c13e6b35e33c23dbe
Iago Tito Oliveira <iagotito@gmail.com>
2022-09-15 20:12:48 -0300

    update cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -11,10 +11,10 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        --['<CR>'] = cmp.mapping.confirm({
-            --behavior = cmp.ConfirmBehavior.Replace,
-            --select = true,
-        --})
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        })
     },
     --sources = cmp.config.sources({
         --{ name = 'nvim_lsp' },
