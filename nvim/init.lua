5b58497f22c64754fe2ab16929825b4b15755136
Iago Tito Oliveira <iagotito@gmail.com>
2022-09-15 23:31:46 -0300

    set default spaces to 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,7 @@ local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local opt = vim.opt
 
+print(vim.bo.filetype)
 -----------------------------------------------------------------------------
 -- General settings
 -----------------------------------------------------------------------------
@@ -16,9 +17,9 @@ opt.visualbell = true -- use a visual bell instead of emit beep
 opt.relativenumber = true -- show relative line distances
 opt.number = true -- show current line number
 opt.scrolloff = 8 -- number of lines offset when jumping
-opt.tabstop = 4 -- number of spaces tabs count for
-opt.softtabstop = 4 -- number of spaces tabs count for while editing
-opt.shiftwidth = 4 -- size of an indent
+opt.tabstop = 2 -- number of spaces tabs count for
+opt.softtabstop = 2 -- number of spaces tabs count for while editing
+opt.shiftwidth = 2 -- size of an indent
 opt.expandtab = true -- use space instead of tabs
 opt.smartindent = true -- insert indents automatically
 opt.showmatch = true -- highlight matching parens, braces, brackets, etc
@@ -43,7 +44,7 @@ cmd("filetype plugin on")
 
 cmd("autocmd BufWritePre * if &ft!='markdown' | let position = winsaveview() | :%s/\\s\\+$//e | call winrestview(position) | unlet! position") -- rstrip white spaces when save except on markdown files
 cmd("autocmd BufNewFile,BufReadPre *.md setlocal textwidth=71 colorcolumn=72") -- auto break line at 71 chars in .md files
-cmd("autocmd BufNewFile,BufReadPre *.html setlocal tabstop=2 softtabstop=2 shiftwidth=2") -- set tab to 2 spaces in .html files
+cmd("autocmd FileType python setlocal tabstop=4 softtabstop=4 shiftwidth=4") -- set tab to 4 spaces in selected filetypes
 
 -- Highlight trailing white spaces
 -- see: https://vim.fandom.com/wiki/Highlight_unwanted_spaces
