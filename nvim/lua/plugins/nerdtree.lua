bbe3d2d304e3febb60a08f761b1e06892f6ba069
Iago Tito Oliveira <iago.tito@pagar.me>
2024-05-15 17:22:41 -0300

    Update NERDTree config
    Show hidden files by default. Ignore '.venv' and '.git' directories.

--- nvim/lua/plugins/nerdtree.lua
+++ nvim/lua/plugins/nerdtree.lua
@@ -7,6 +7,12 @@ vim.cmd([[autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NER
 -- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
 vim.cmd([[autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif]])
 
+-- Show hidden files by default
+vim.g.NERDTreeShowHidden = 1
+
+-- Ignore patterns
+vim.g.NERDTreeIgnore = {'.venv$[[dir]]', '.git$[[dir]]'}
+
 -- Keymaps
 require'keymaps.util'
 
