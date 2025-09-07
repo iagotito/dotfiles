a6300f2c45edea49bac4cd8bb35904551f1a2113
Iago Tito <iagotito@gmail.com>
2025-09-07 18:40:34 -0300

    ✨ Add debug plugin

---
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-telescope/telescope-dap.nvim",
+			"mfussenegger/nvim-dap-python",
+			--"leoluz/nvim-dap-go",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-python").setup()
+			--require("dap-go").setup()
+
+			require("nvim-dap-virtual-text").setup({})
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F2>", dap.step_back)
+			vim.keymap.set("n", "<F3>", dap.continue)
+			vim.keymap.set("n", "<F4>", dap.step_over)
+			vim.keymap.set("n", "<F7>", dap.step_out)
+			vim.keymap.set("n", "<F8>", dap.step_into)
+			vim.keymap.set("n", "<F12>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
