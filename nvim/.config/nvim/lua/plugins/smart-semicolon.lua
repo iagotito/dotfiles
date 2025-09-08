35e11a7f269b18bb5059dcb6baa7e14d85475bed
Iago Tito <iagotito@gmail.com>
2025-09-08 08:52:19 -0300

    🐛 Fix smart-semicolon plugin import

--- nvim/.config/nvim/lua/plugins/smart-semicolon.lua
+++ nvim/.config/nvim/lua/plugins/smart-semicolon.lua
@@ -1,6 +1,5 @@
 return {
-	dir = "/home/iago/Documents/smart-semicolon.nvim",
-	name = "smart-semicolon",
+	"git@github.com:iagotito/smart-semicolon.nvim.git",
 	config = function()
 		require("smart_semicolon").setup()
 	end,
